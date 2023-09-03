17e8e1597e5248d746389da9fccd7de97c789740
lavanyaboddu31@gmail.com <lavanyaboddu31@gmail.com>
2023-09-03 16:25:28 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+using Using_Custom_Filters.Models;
+using static Using_Custom_Filters.Controllers.HomeController2;
+
+namespace Using_Custom_Filters.Controllers
+{
+   
+        [ActionLoggingFilter]
+        public class HomeController : Controller
+        {
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            public IActionResult About()
+            {
+                ViewData["Message"] = "Your application description page.";
+
+                return View();
+            }
+
+            public IActionResult Contact()
+            {
+                ViewData["Message"] = "Your contact page.";
+
+                return View();
+            }
+
+            public IActionResult Privacy()
+            {
+                return View();
+            }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+            [AllowAnonymous]
+            public ViewResult StudentView()
+            {
+                return View();
+            }
+
+            public ViewResult Login()
+            {
+                return View();
+
+            }
+
+            [Authorize]
+            public ViewResult StudentViewSecure()
+            {
+                return View();
+
+            }
+
+            public IActionResult HomeLoginAction()
+            {
+                string userName = "admin";
+
+                var identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userName)
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var principal = new ClaimsPrincipal(identity);
+
+                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                return RedirectToAction("StudentViewSecure", "Home");
+            }
+        }
+    }
---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Using_Custom_Filters.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public class AccountController : Controller
+        {
+            public IActionResult Login()
+            {
+                return View();
+            }
+        }
+    }
+}
+
